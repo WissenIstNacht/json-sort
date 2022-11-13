2b25813855560ab331c24ed7f7a917db217186c4
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2022-11-13 23:10:35 +0100

    improve console output

--- index.js
+++ index.js
@@ -3,17 +3,19 @@ const sorter = require("json-keys-sort")
 
 const args = process.argv
 const path = args[2]
+const LCINFO = "\x1b[36m%s\x1b[0m" //cyan
 
-console.info("Reading Json from file at: " + path)
+console.info(LCINFO, "Reading Json from file at: " + path)
 
 fs.readFile(path)
 	.then((data) => {
 		const json = JSON.parse(data)
-		console.log("The following object was read from file:")
+		console.log(LCINFO, "The following object was read from file:")
 		console.log(json)
 		return sorter.sort(json, true)
 	})
 	.then((json_sorted) => {
+		console.log(LCINFO, "This object will be written to file:")
 		console.log(json_sorted)
 		const jsonText = JSON.stringify(json_sorted, null, 2)
 		fs.writeFile(path, jsonText)
