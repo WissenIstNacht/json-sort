fe9fca898c4b308bfc16109a8341176b464185e4
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2022-11-13 22:54:54 +0100

    move to node's promise-based fs

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
-const fs = require("fs")
+const fs = require("fs/promises")
 const sorter = require("json-keys-sort")
-
-console.log("hi")
 
 const args = process.argv
 const path = args[2]
-console.log(path)
 
-fs.readFile(path, (err, data) => {
-    const json = JSON.parse(data)
-    console.log(json)
-    const json_sorted = sorter.sort(json, true)
-    console.log(json_sorted)
-    const jsonText = JSON.stringify(json_sorted, null, 2)
-    fs.writeFile(path, jsonText, () => console.log("DONE!"))
-})
+console.info("Reading Json from file at: " + path)
+
+fs.readFile(path)
+	.then((data) => {
+		const json = JSON.parse(data)
+		console.log("The following object was read from file:")
+		console.log(json)
+		return sorter.sort(json, true)
+	})
+	.then((json_sorted) => {
+		console.log(json_sorted)
+		const jsonText = JSON.stringify(json_sorted, null, 2)
+		fs.writeFile(path, jsonText)
+	})
+	.then(() => {
+		console.log("DONE!")
+	})
+	.catch((e) => console.error("The following error occurred", e))
