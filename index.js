b1fb57e70b6e559ef64c3dcdf26e487d5f181584
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2022-11-15 16:32:27 +0100

    improving output

--- index.js
+++ index.js
@@ -7,29 +7,38 @@ const pathOut = args[3]
 const LCINFO = "\x1b[36m%s\x1b[0m" //cyan
 
 console.info(LCINFO, "Reading Json from file at: " + path)
+console.log("")
 
 fs.readFile(path)
 	.then((data) => {
 		const json = JSON.parse(data)
 		console.log(LCINFO, "The following object was read from file:")
 		console.log(json)
+		console.log("")
+
 		return sorter.sort(json, true)
 	})
 	.then((json_sorted) => {
 		console.log(LCINFO, "This object will be written to file:")
 		console.log(json_sorted)
+		console.log("")
+
 		const jsonText = JSON.stringify(json_sorted, null, 2)
 		if (!pathOut) {
 			const parts = path.split(".json")
 			const copiedPath = parts[0] + "_copy.json"
 			console.info(LCINFO, "Writing Json from file at: " + copiedPath)
+			console.log("")
+
 			return fs.writeFile(copiedPath, jsonText)
 		} else {
 			console.info(LCINFO, "Writing Json from file at: " + pathOut)
+			console.log("")
+
 			return fs.writeFile(pathOut, jsonText)
 		}
 	})
 	.then(() => {
-		console.log("DONE!")
+		console.log(LCINFO, "DONE!")
 	})
 	.catch((e) => console.error("The following error occurred", e))
