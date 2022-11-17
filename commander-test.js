1963ad72df9be3b588b40920dd948bcca99e26a1
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2022-11-17 11:10:59 +0100

    delete commander POC

--- commander-test.js
+++
@@ -1,8 +0,0 @@
-const { program } = require('commander');
-
-program
-  .name('JSON Sorter')
-  .argument('<inputPath>', 'Path of JSON-file that needs sorting');
-
-program.parse(process.argv);
-console.log(program.args);
