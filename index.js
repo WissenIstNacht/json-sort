c6e797a3aafe073934b48dc369b129109af48703
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2022-11-17 11:08:22 +0100

    Add commander for CLI

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const fs = require("fs/promises")
 const sorter = require("json-keys-sort")
+const { program } = require("commander")
 
-const args = process.argv
-const path = args[2]
-const pathOut = args[3]
+program
+	.name("JSON Sorter")
+	.description("This program sorts JSON files by keys.")
+	.argument("<inputPath>", "Path of JSON-file that needs sorting")
+	.option(
+		"-o, --out <path>",
+		`Path of the JSON-file to which the output will be written. If the file already exists, a copy will be generated. If not specified a copy of the input file will be generated.`
+	)
+
+program.parse()
+console.log(program.opts())
+const pathIn = program.args[0]
+const pathOut = program.opts().out
 const LCINFO = "\x1b[36m%s\x1b[0m" //cyan
 
-console.info(LCINFO, "Reading Json from file at: " + path)
+console.info(LCINFO, "Reading Json from file at: " + pathIn)
 console.log("")
 
-fs.readFile(path)
+fs.readFile(pathIn)
 	.then((data) => {
 		const json = JSON.parse(data)
 		console.log(LCINFO, "The following object was read from file:")
@@ -25,7 +36,7 @@ fs.readFile(path)
 
 		const jsonText = JSON.stringify(json_sorted, null, 2)
 		if (!pathOut) {
-			const parts = path.split(".json")
+			const parts = pathIn.split(".json")
 			const copiedPath = parts[0] + "_copy.json"
 			console.info(LCINFO, "Writing Json from file at: " + copiedPath)
 			console.log("")
